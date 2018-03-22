7d215b9ae91a4224c3f6fc2b182be3f659a64041
Lucamdk <luca.miraglia.d@gmail.com>
2018-03-22 01:52:05 -0300

    Otras validaciones de edicion

--- graboPublicacionEditada.php
+++ graboPublicacionEditada.php
@@ -46,57 +46,64 @@ if ($estado == TRUE) {
 
 if (in_array($ext, $extensiones)) {
 
+    if (!(empty($pubNombre)) and ! (empty($pubDesc))) {
+
 
 
 //veo que puedo conectarme a la BD
-    if ($conn->conectar()) {
-        $sql1 = "SELECT id FROM Usuarios WHERE email = '" . $usuNom . "'";
-        $parametros = array();
-
-
-        if ($conn->consulta($sql1, $parametros)) {
-            $usuDatos = $conn->siguienteRegistro();
-            $usuId = (int) $usuDatos['id'];
-
-            //armo la SQL
-            $sql = "UPDATE Publicaciones SET titulo=:nom, descripcion=:desc, tipo=:est,"
-                    . "abierto=:abierto, usuario_id=:usuId, exitoso=:exitoso WHERE id=$pubId";
-
-
-            $sql2 = "INSERT INTO Publicaciones_fotos (id_publicacion, pubFoto) VALUES (:id, :rutaFoto)";
-
-
-            //cargo los parametros para la sql
+        if ($conn->conectar()) {
+            $sql1 = "SELECT id FROM Usuarios WHERE email = '" . $usuNom . "'";
             $parametros = array();
-            $parametros[0] = array("nom", $pubNombre, "string");
-            $parametros[1] = array("desc", $pubDesc, "string");
-            $parametros[2] = array("est", $estado, "string");
-            $parametros[6] = array("abierto", $abierto, "int");
-            $parametros[7] = array("usuId", $usuId, "int");
-            $parametros[8] = array("exitoso", $exitoso, "int");
+
+
+            if ($conn->consulta($sql1, $parametros)) {
+                $usuDatos = $conn->siguienteRegistro();
+                $usuId = (int) $usuDatos['id'];
+
+                //armo la SQL
+                $sql = "UPDATE Publicaciones SET titulo=:nom, descripcion=:desc, tipo=:est,"
+                        . "abierto=:abierto, usuario_id=:usuId, exitoso=:exitoso WHERE id=$pubId";
+
+
+                $sql2 = "INSERT INTO Publicaciones_fotos (id_publicacion, pubFoto) VALUES (:id, :rutaFoto)";
+
+
+                //cargo los parametros para la sql
+                $parametros = array();
+                $parametros[0] = array("nom", $pubNombre, "string");
+                $parametros[1] = array("desc", $pubDesc, "string");
+                $parametros[2] = array("est", $estado, "string");
+                $parametros[6] = array("abierto", $abierto, "int");
+                $parametros[7] = array("usuId", $usuId, "int");
+                $parametros[8] = array("exitoso", $exitoso, "int");
 //        $parametros[9] = array("rutFoto", $foto, "string");
 //        echo "<pre>";
 //        print_r($parametros);
 //        echo "<pre>";
-            if ($nuevaFoto == TRUE) {
-                $parametros2 = array();
-                $parametros2[0] = array("id", $pubId, "int");
-                $parametros2[1] = array("rutaFoto", $foto, "string");
-                if ($conn->consulta($sql2, $parametros2)) {
+                if ($nuevaFoto == TRUE) {
+                    $parametros2 = array();
+                    $parametros2[0] = array("id", $pubId, "int");
+                    $parametros2[1] = array("rutaFoto", $foto, "string");
+                    if ($conn->consulta($sql2, $parametros2)) {
+                        echo "Error de Consulta de insersion" . $conn->ultimoError();
+                    }
+                }
+
+                if ($conn->consulta($sql, $parametros)) {
+                    header("Location: index.php");
+                } else {
                     echo "Error de Consulta de insersion" . $conn->ultimoError();
                 }
-            }
-
-            if ($conn->consulta($sql, $parametros)) {
-                header("Location: index.php");
             } else {
-                echo "Error de Consulta de insersion" . $conn->ultimoError();
+                echo "Error de Consulta 1 " . $conn->ultimoError();
             }
         } else {
-            echo "Error de Consulta 1 " . $conn->ultimoError();
+            echo "Error de Conexión " . $conn->ultimoError();
         }
     } else {
-        echo "Error de Conexión " . $conn->ultimoError();
+        $smarty = new Smarty();
+        $_SESSION['mensaje'] = "No pueden haber campos vacíos";
+        header("Location: editarPublicacion.php");
     }
 } else {
     $smarty = new Smarty();
